a954fd795b07939736272db2e1e7ecdd9fdb417f
MB-study <MB-study@192.168.1.96>
2020-11-03 20:52:28 +0800

    Java

---
+++ src/main/java/com/ocp/Calc.java
@@ -0,0 +1,10 @@
+package com.ocp;
+
+public class Calc {
+    public static void main(String[] args) {
+        Area a1 = new Area();
+        a1.h = 100;
+        a1.w = 50;
+        System.out.println(a1.h * a1.w);
+    }
+}
