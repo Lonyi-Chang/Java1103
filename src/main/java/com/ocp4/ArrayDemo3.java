378bd4d1acc2758126d351217b9f3fd8145cc9f8
MB-study <MB-study@192.168.1.96>
2020-11-12 21:39:39 +0800

    Java

---
+++ src/main/java/com/ocp4/ArrayDemo3.java
@@ -0,0 +1,13 @@
+package com.ocp4;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArrayDemo3 {
+    public static void main(String[] args) {
+        int[] nums = RandomNumber.getRandomNumber(4);
+        System.out.println(Arrays.toString(nums));
+    }
+}
+    
+   
