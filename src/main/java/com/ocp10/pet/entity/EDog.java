1f2e5cb5d0adb352efc6652e34a08c66d3039879
MB-study <MB-study@192.168.1.96>
2020-12-03 21:48:40 +0800

    Java

---
+++ src/main/java/com/ocp10/pet/entity/EDog.java
@@ -0,0 +1,20 @@
+package com.ocp10.pet.entity;
+
+public class EDog extends Dog{
+    private int price;
+    
+    public EDog(int price) {
+        this.price = price;
+    }
+    
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public void eat() {
+         System.out.println("吃3號電池兩顆");
+    }
+    
+}
